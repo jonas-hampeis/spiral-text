8b145205c60b0f1bf19ef21b3263bf0dadee1f06
Jonas Hampeis <jonas@hampeis.cz>
2019-12-02 15:39:01 +0100

    Spiral text works now, needs few tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,30 +28,55 @@ export class AppComponent implements AfterViewInit {
 
     ctx.translate(x,y);
 
-    ctx.fillRect(0,0,3,3);
+    let rIter = 50;
 
-    ctx.font = 50 + 'px ' + 'Arial';
+    ctx.font = rIter + 'px ' + 'Arial';
     ctx.lineWidth = 2;
     ctx.textBaseline = 'middle';
     ctx.textAlign = 'center';
 
-    let rIter = 50;
     let inc = rIter / 2;
     let move = false;
+    let r = rIter;
+    let cor = 0;
 
-    var rText = 'Rotation goes around and around and around';
+    var rText = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer in sapien. In dapibus augue non sapien. Nam sed tellus id magna elementum tincidunt. Aenean fermentum risus id tortor.';
 
     let text: Letter[] = [];
 
-    let cWidth = ctx.measureText(rText[i]).width;
     let angle = -Math.PI
-    text[0] = new Letter(rText[0], -50, 0, angle, angle, cWidth, false);
+    text[0] = new Letter(rText[0], -rIter, 0, angle, false);
 
     for(var i = 1; i < rText.length; ++i){
       
       let cWidth = ctx.measureText(rText[i]).width;
-
       angle += 2 * (Math.asin(cWidth/(2*rIter)));
+      angle += cor;
+      cor = 0;
+
+      let angset = 0;
+      if(rText[i] === "f" || rText[i] === "r"){
+        angset = 1.5;
+      } else if(rText[i] === "i" || rText[i] === "j" || rText[i] === "l" || rText[i] === "t" || rText[i] === "I"){
+        angset = 2.5
+      } else if(rText[i] === "m" || rText[i] === "w"){
+        angset = -2.5
+      } else if(rText[i] === "." || rText[i] === ","){
+        angset = 0.5
+      }
+      angle += Math.PI/(rIter/angset);
+
+      let corset = 0;
+      if(rText[i] === "f" || rText[i] === "i" || rText[i] === "j" || rText[i] === "I"){
+        corset = -2.5;
+      } else if(rText[i] === "m" || rText[i] === "w"){
+        corset = +2;
+      } else if(rText[i] === "l"){
+        corset = -2.8;
+      } else if(rText[i] === "r" || rText[i] === "t"){
+        corset = -1.5;
+      }
+      cor = Math.PI/(rIter/corset);
       
       let cy = rIter * Math.sin(angle) - Math.PI/2;
 
@@ -65,35 +90,32 @@ export class AppComponent implements AfterViewInit {
 
       let cx = rIter * Math.cos(angle) - Math.PI/2;
 
-      text.push(new Letter(rText[i], cx, cy, angle, 0, cWidth, move));
+      text.push(new Letter(rText[i], cx, cy, angle, move));
       
       console.log(text[i].name + ": \n" + 
-                  "Width: " + text[i].width + 
+                  "Width: " + cWidth + 
                   "\nx: " + text[i].x  +
                   "\ny: " + text[i].y  +
-                  "\nangle: " + text[i].angle  +
-                  "\nrotation: " + text[i].rot );
+                  "\nangle: " + text[i].angle);
 
-    }
-    
 
-    let r = 50;
-
-    for(let i = 0; i < text.length; ++i){
 
       if(text[i].move){
         text[i].x -= r/2;
       }
-      //ctx.rotate(text[i].rot + Math.PI/2);
-      ctx.fillText(text[i].name, text[i].x, text[i].y );
 
     }
-    ctx.save();
 
-    //ctx.rotate(text[0].rot + Math.PI/2);
-    ctx.fillText(text[0].name, text[0].x, text[0].y );
 
-    ctx.restore();
+    for(let i = 0; i < text.length; ++i){
+      ctx.save();
+      
+      ctx.translate(text[i].x, text[i].y )
+      ctx.rotate(text[i].angle + Math.PI/2);
+      ctx.fillText(text[i].name, 0, 0 );
+
+      ctx.restore();
+    }
   }
 
   private drawSpiral() {
@@ -140,15 +162,11 @@ export class Letter {
   x: number;
   y: number;
   angle: number;
-  rot: number;
-  width: number;
   move: boolean;
-  public constructor(name: string, x: number, y: number, angle:number, rot: number, width: number, move: boolean) {
+  public constructor(name: string, x: number, y: number, angle:number, move: boolean) {
     this.x = x;
     this.y = y;
-    this.rot = angle;
     this.name = name;
-    this.width = width;
     this.angle = angle;
     this.move = move;
   } 
