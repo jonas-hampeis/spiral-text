1d910dd3455ef397e43ca70d6cfc4e33e5209489
Jonas Hampeis <jonas@hampeis.cz>
2019-12-05 21:13:00 +0100

    Added two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { sanitizeIdentifier } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -13,18 +12,31 @@ export class AppComponent implements AfterViewInit {
   constructor() {}
   ngAfterViewInit() {
     this.context = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d');
-  
-    let roti = -0.25;
-    //this.drawSpiral();
-    this.drawText();
+    this.drawText(this.rText);
   }
 
-  private drawText() {
+  rText = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris elementum mauris vitae tortor. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Aliquam ornare wisi eu metus. Fusce tellus. Phasellus rhoncus.';
+
+   getCurrentModel() { 
+    //this.drawText(this.rText);
+    return JSON.stringify(this.rText); 
+   }
+
+  dataChanged() {
+    this.drawText(this.rText);
+  }
+
+  public drawText(tex: string) {
 
     let ctx = this.context; 
+    ctx.canvas.width  = window.innerHeight -20;
+    ctx.canvas.height = window.innerHeight -20;
+    ctx.save();
 
     const x = (this.canvasEl.nativeElement as HTMLCanvasElement).width / 2;
     const y = (this.canvasEl.nativeElement as HTMLCanvasElement).height / 2;
+
+    ctx.clearRect(0, 0, 2*x, 2*y);
 
     ctx.translate(x,y);
 
@@ -40,47 +52,18 @@ export class AppComponent implements AfterViewInit {
     let r = rIter;
     let cor = 0;
 
-    var rText = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris elementum mauris vitae tortor. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Aliquam ornare wisi eu metus. Fusce tellus. Phasellus rhoncus.';
 
     let text: Letter[] = [];
 
     let angle = -Math.PI
-    text[0] = new Letter(rText[0], -rIter, 0, angle, false);
+    text[0] = new Letter(tex[0], -rIter, 0, angle, false);
 
-    for(var i = 1; i < rText.length; ++i){
+    for(var i = 1; i < tex.length; ++i){
       
-      let cWidth = ctx.measureText(rText[i]).width;
+      let cWidth = ctx.measureText(tex[i]).width;
       angle += 2 * (Math.asin(cWidth/(2*rIter)));
       angle += cor;
       cor = 0;
-
-      // let angset = 0;
-      // if(rText[i] === "f" || rText[i] === "r" || rText[i] === "ř"){
-      //   angset = 1.5;
-      // } else if(rText[i] === "i" || rText[i] === "j" || rText[i] === "l" || rText[i] === "t" || rText[i] === "I" || rText[i] === "í"){
-      //   angset = 0;
-      // } else if( rText[i] === "w" || rText[i] === "M"){
-      //   angset = -2.5
-      // } else if(rText[i] === "." || rText[i] === "," || rText[i] === "y"){
-      //   angset = 0.5
-      // } else if(rText[i] === "m" || rText[i] === "H"){
-      //   angset = 0;
-      // }
-      // angle += Math.PI/(rIter/angset);
-
-      // let corset = 0;
-      // if(rText[i] === "f" || rText[i] === "i" || rText[i] === "j" || rText[i] === "I" || rText[i] === "í"){
-      //   corset = 0;
-      // } else if(rText[i] === "m" || rText[i] === "w"){
-      //   corset = +2;
-      // } else if(rText[i] === "l"){
-      //   corset = 0;
-      // } else if(rText[i] === "r" || rText[i] === "t" || rText[i] === "ř"){
-      //   corset = 0;
-      // } else if(rText[i] === "M" || rText[i] === "H"){
-      //   corset = +1.1;
-      // }
-      // cor = Math.PI/(rIter/corset);
       
       let cy = rIter * Math.sin(angle) - Math.PI/2;
 
@@ -94,7 +77,7 @@ export class AppComponent implements AfterViewInit {
 
       let cx = rIter * Math.cos(angle) - Math.PI/2;
 
-      text.push(new Letter(rText[i], cx, cy, angle, move));
+      text.push(new Letter(tex[i], cx, cy, angle, move));
       
       console.log(text[i].name + ": \n" + 
                   "Width: " + cWidth + 
@@ -120,44 +103,7 @@ export class AppComponent implements AfterViewInit {
 
       ctx.restore();
     }
-  }
-
-  private drawSpiral() {
-
-    let ctx = this.context; 
-
-    const x = (this.canvasEl.nativeElement as HTMLCanvasElement).width / 2;
-    const y = (this.canvasEl.nativeElement as HTMLCanvasElement).height / 2 - 8;
-
-    //setup
-    let r = 50;
-    let inc = r / 2;
-    let size = 8;
-    let st;
-    let end;
-    let move = 0;
-
-    for(let i = 0; i < size; ++i){
-      ctx.beginPath();
-
-      let radius = r + (i * inc);
-      
-      if (i % 2 == 0){
-        st = Math.PI;
-        end = 2*Math.PI;
-      }else{
-        st = 0;
-        end = Math.PI;
-        move = inc;
-      }
-      
-      ctx.arc(x - move, y, radius, st, end);
-
-      ctx.stroke();
-      move = 0;
-    }
-
-
+    ctx.restore();
   }
 }
 
