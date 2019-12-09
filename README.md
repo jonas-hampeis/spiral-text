4c23f3730ffabb01f6f8ab61c28ff7e23c5edbdc
Jonas Hampeis <jonas@hampeis.cz>
2019-12-09 15:24:45 +0100

    Added customization settings and some responsivenes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # SpiralText
 
-This project generates a spiral from a string.
+This project generates a spiral from a string using HTML canvas and some JS.
 
-
+You can preview it here: https://jonas-hampeis.github.io/spiral-text/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import {Letter} from './letter'
 
 @Component({
   selector: 'app-root',
@@ -10,27 +11,58 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('canvasEl', {static: false}) canvasEl: ElementRef;
   private context: CanvasRenderingContext2D;
   constructor() {}
+  ngOnInit() {
+    this.initVarValues();
+  }
   ngAfterViewInit() {
     this.context = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d');
-    this.drawText(this.rText);
+    
+    this.drawText(this.rText, this.fontFa, this.screenSize());
   }
 
   rText = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris elementum mauris vitae tortor. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Aliquam ornare wisi eu metus. Fusce tellus. Phasellus rhoncus.';
+  fontSz: number;
+  fontFa = "Courier New";
+  ctxSize: number;
 
-   getCurrentModel() { 
-    //this.drawText(this.rText);
-    return JSON.stringify(this.rText); 
-   }
-
-  dataChanged($event: any) {
-    this.drawText(this.rText);
+  initVarValues(){
+    if(window.screen.height < window.screen.width){
+      //Computer screen
+      this.ctxSize = window.screen.height -120;
+      this.fontSz = 35;
+    } else{
+      //phone screen
+      this.ctxSize = window.screen.width -20;
+      this.fontSz = 20;
+    }
   }
 
-  public drawText(tex: string) {
+  screenSize(){
+    if(window.screen.height < window.screen.width){
+      var ctxSize = window.screen.height -120;
+    } else{
+      var ctxSize = window.screen.width -20;
+    }
+
+    return ctxSize;
+  }
+
+  dataChanged($event: any) {
+    this.drawText(this.rText, this.fontFa, this.ctxSize);
+  }
+
+  public drawText(tex: string, fontFam: string, canvasSz: number) {
+
+    if(tex == "") {
+      tex = "Please write something.";
+    }
+    if(this.fontFa == "") {
+     fontFam = "Courier New";
+    }
 
     let ctx = this.context; 
-    ctx.canvas.width  = window.innerHeight -20;
-    ctx.canvas.height = window.innerHeight -20;
+    ctx.canvas.width  = canvasSz;
+    ctx.canvas.height = canvasSz;
     ctx.save();
 
     const x = (this.canvasEl.nativeElement as HTMLCanvasElement).width / 2;
@@ -40,9 +72,9 @@ export class AppComponent implements AfterViewInit {
 
     ctx.translate(x,y);
 
-    let rIter = 35;
+    let rIter = this.fontSz;
 
-    ctx.font = rIter + 'px ' + 'Courier New';
+    ctx.font = rIter + 'px ' + fontFam;
     ctx.lineWidth = 2;
     ctx.textBaseline = 'middle';
     ctx.textAlign = 'center';
@@ -50,40 +82,44 @@ export class AppComponent implements AfterViewInit {
     let inc = rIter / 2;
     let move = false;
     let r = rIter;
-    let cor = 0;
 
 
     let text: Letter[] = [];
 
-    let angle = -Math.PI
-    text[0] = new Letter(tex[0], -rIter, 0, angle, false);
+    let angle = -Math.PI - (Math.asin(rIter/(2*rIter)));
 
-    for(var i = 1; i < tex.length; ++i){
+    for(var i = 0; i < tex.length; ++i){
       
-      let cWidth = ctx.measureText(tex[i]).width;
+      if(tex[i-1]) {
+        var cWidth = ( ctx.measureText(tex[i]).width / 2 ) + ( ctx.measureText(tex[i-1]).width / 2 );
+      } else {
+        var cWidth = ctx.measureText(tex[i]).width; 
+      }
+
       angle += 2 * (Math.asin(cWidth/(2*rIter)));
-      angle += cor;
-      cor = 0;
       
       let cy = rIter * Math.sin(angle) - Math.PI/2;
 
-      if(cy > 0 && text[i-1].y < 0) {
-        rIter = rIter + inc;
-        move = true;
-      }else if (cy < 0 && text[i-1].y > 0){
-        rIter = rIter + inc;
-        move = false;
+      if(text[i-1]) {
+        if(cy >= 0 && text[i-1].y <= 0) {
+          rIter = rIter + inc;
+          move = true;
+        }else if (cy < 0 && text[i-1].y > 0){
+          rIter = rIter + inc;
+          move = false;
+        }
       }
 
       let cx = rIter * Math.cos(angle) - Math.PI/2;
 
       text.push(new Letter(tex[i], cx, cy, angle, move));
       
-      console.log(text[i].name + ": \n" + 
-                  "Width: " + cWidth + 
-                  "\nx: " + text[i].x  +
-                  "\ny: " + text[i].y  +
-                  "\nangle: " + text[i].angle);
+      //console log for viewing the positions of the letters
+      // console.log(text[i].name + ": \n" + 
+      //             "Width: " + cWidth + 
+      //             "\nx: " + text[i].x  +
+      //             "\ny: " + text[i].y  +
+      //             "\nangle: " + text[i].angle);
 
 
 
@@ -94,30 +130,18 @@ export class AppComponent implements AfterViewInit {
     }
 
 
-    for(let i = 0; i < text.length; ++i){
+    //drawing function
+    text.forEach(function(i){
       ctx.save();
       
-      ctx.translate(text[i].x, text[i].y )
-      ctx.rotate(text[i].angle + Math.PI/2);
-      ctx.fillText(text[i].name, 0, 0 );
+      ctx.translate(i.x, i.y )
+      ctx.rotate(i.angle + Math.PI/2);
+      ctx.fillText(i.name, 0, 0 );
 
       ctx.restore();
-    }
+    });
+
+    //restores the canvas so it doesn't get offset after redrawing
     ctx.restore();
   }
-}
-
-export class Letter {
-  name: string;
-  x: number;
-  y: number;
-  angle: number;
-  move: boolean;
-  public constructor(name: string, x: number, y: number, angle:number, move: boolean) {
-    this.x = x;
-    this.y = y;
-    this.name = name;
-    this.angle = angle;
-    this.move = move;
-  } 
 }
